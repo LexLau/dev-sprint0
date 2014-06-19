e04282ec003ce93dacad2e6151cf229e8469d3b7
Alex Lau <aznalix@yahoo.com>
2014-06-18 21:17:52 -0400

    finished flower

--- swampy-package/swampy/sib_flower.py
+++ swampy-package/swampy/sib_flower.py
@@ -1,22 +1,40 @@
 # Flower excercise (4.2) from Week 0
 
-# Name:
+# Name: Alex Lau
 
+from sib_polygon import *
 
 from TurtleWorld import * 		
 world = TurtleWorld()			
 bob = Turtle()				
-
-
+bob.delay = 0.005
 
 # This is where you put code to move bob
+def petal(t, r, angle):
+	for i in range(2):
+		arc(t, r, angle)
+		lt(t, 180-angle)
 
+def flower(t, n, r, angle):
+	for i in range(n):
+		petal(t, r, angle)
+		lt(t, 360.0/n)
 
+def move(t, length):
+	pu(t)
+	fd(t, length)
+	pd(t)
 
+move(bob, -100)
+flower(bob, 7, 60.0, 60.0)
 
+move(bob, 100)
+flower(bob, 10, 40.0, 80.0)
 
+move(bob, 100)
+flower(bob, 20, 140.0, 20.0)
 
-
+die(bob)
 
 wait_for_user()					
 
--- swampy-package/swampy/sib_polygon.py
+++ swampy-package/swampy/sib_polygon.py
@@ -1,20 +1,48 @@
 # Polygon excercise from Week 0
 
-# Name:
+# Name: Alex Lau
 
-
+import math
 from TurtleWorld import * 		
 world = TurtleWorld()			
 bob = Turtle()				
-
+bob.delay = 0.005
 
 
 # This is where you put code to move bob
+def square(t, length):
+	for i in range(4):
+		fd(t, length)
+		lt(t)
 
+def polyline(t, n, length, angle):
+	for i in range(n):
+		fd(t, length)
+		lt(t, angle)
 
+def polygon(t, n, length):
+	angle = 360.0/n
+	polyline(t, n, length, angle)
 
+def arc(t, r, angle):
+	arc_length = 2 * math.pi * r * abs(angle) / 360
+	n = int(arc_length / 4 + 1)
+	step_length = arc_length / n
+	step_angle = float(angle) / n
+	# make a slight left to make a better arc
+	lt(t, step_angle/2)
+	polyline(t, n, step_length, step_angle)
+	rt(t, step_angle/2)
 
+def circle(t, r):
+	arc(t, r, 360)
 
+radius = 100
+pu(bob)
+fd(bob, radius)
+lt(bob)
+pd(bob)
+circle(bob, radius)
 
 
 
